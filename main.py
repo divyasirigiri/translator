4508ac51c8c153b55a2dcfc43ba8bed1fdab07af
Divya Sirigiri <divyasirigiri7@gmail.com>
2021-07-11 15:34:05 +0530

    modified

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from flask import Flask, render_template, request
 from textblob import TextBlob
 from textblob import Word
 
-app = Flask(__name__)
+app1 = Flask(__name__)
 
 
-@app.route('/')
+@app1.route('/')
 def hello():
     return render_template('base.html')
 
-@app.route('/define',methods = ['POST'])
+@app1.route('/define',methods = ['POST'])
 def define():
     ip_word = request.form.get('ip-word')
     
@@ -19,4 +19,4 @@ def define():
     return render_template('base.html',prediction_text=f'\n the meaning of the word {(ip_word)}  is : {res}')
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app1.run(debug=True)
