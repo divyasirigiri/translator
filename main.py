a1851f45de0f9a3de6a8e406ef789ef8dd61172a
divyasirigiri <44464023+divyasirigiri@users.noreply.github.com>
2021-09-17 16:38:44 +0530

    Add files via upload
    modified

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from logging import debug
 from flask import Flask, render_template, request
 
+from translate import Translator
 from textblob import TextBlob
 from textblob import Word
 
@@ -13,20 +14,24 @@ def hello():
 
 @app1.route('/define',methods = ['POST'])
 def define():
-    ip_word = request.form.get('ip-word')
-    # lang1 = request.form.get('lang1')
-    lang2 = request.form.get('lang2')
-    temp=['af','sq','am','ar','hy','az','eu','be','bn','bs','bg','ca','ceb','zh-CN','zh-TW','co','hr','cs','da','nl','en','eo','et','fi','fr','fy','gl','ka','de','el','gu','ht','ha','haw','he','hi','hmn','hu','is','ig','id','ga','it','ja','jv','kn','kk','km','rw','ko','ku','ky','lo','la','lv','lt','lb','mk','mg','ms','ml','mt','mi','mr','mn','my','ne','no','ny','or','ps','fa','pl','pt','pa','ro','ru','sm','gd','sr','st','sn','sd','si','sk','sl','so','es','su','sw','sv','tl','tg','ta','tt','te','th','tr','tk','uk','ur','ug','uz','vi','cy','xh','yi','yo','zu']
+    try:
+        ip_word = request.form.get('ip-word')
+        # lang1 = request.form.get('lang1')
+        lang2 = request.form.get('lang2')
+        temp=['af','sq','am','ar','hy','az','eu','be','bn','bs','bg','ca','ceb','zh-CN','zh-TW','co','hr','cs','da','nl','en','eo','et','fi','fr','fy','gl','ka','de','el','gu','ht','ha','haw','he','hi','hmn','hu','is','ig','id','ga','it','ja','jv','kn','kk','km','rw','ko','ku','ky','lo','la','lv','lt','lb','mk','mg','ms','ml','mt','mi','mr','mn','my','ne','no','ny','or','ps','fa','pl','pt','pa','ro','ru','sm','gd','sr','st','sn','sd','si','sk','sl','so','es','su','sw','sv','tl','tg','ta','tt','te','th','tr','tk','uk','ur','ug','uz','vi','cy','xh','yi','yo','zu']
+        
+        k = ['Afrikaans','Albanian','Amharic','Arabic','Armenian','Azerbaijani','Basque','Belarusian','Bengali','Bosnian','Bulgarian','Catalan','Cebuano','Chinese (Simplified)','Chinese (Traditional)','Corsican','Croatian','Czech','Danish','Dutch','English','Esperanto','Estonian','Finnish','French','Frisian','Galician','Georgian','German','Greek','Gujarati','Haitian Creole','Hausa','Hawaiian','Hebrew','Hindi','Hmong','Hungarian','Icelandic','Igbo','Indonesian','Irish','Italian','Japanese','Javanese','Kannada','Kazakh','Kazakhmer','Kinyarwanda','Korean','Kurdish','Kyrgyz','Lao','Latin','Latvian','Lithuanian','Luxembourgish','Macedonian','Malagasy','Malay','Malayalam','Maltese','Maori','Marathi','Mongolian','Myanmar (Burmese)','Nepali','Norwegian','Nyanja (Chichewa)','Odia (Oriya)','Pashto','Persian','Polish','Portuguese (Portugal, Brazil)','Punjabi','Romanian','Russian','Samoan','Scots Gaelic','Serbian','Sesotho','Shona','Sindhi','Sinhala (Sinhalese)','Slovak','Slovenian','Somali','Spanish','Sundanese','Swahili','Swedish','Tagalog (Filipino)','Tajik','Tamil','Tatar','Telugu','Thai','Turkish','Turkmen','Ukrainian','Urdu','Uyghur','Uzbek','Vietnamese','Welsh','Xhosa','Yiddish','Yoruba','Zulu']
     
-    k = ['Afrikaans','Albanian','Amharic','Arabic','Armenian','Azerbaijani','Basque','Belarusian','Bengali','Bosnian','Bulgarian','Catalan','Cebuano','Chinese (Simplified)','Chinese (Traditional)','Corsican','Croatian','Czech','Danish','Dutch','English','Esperanto','Estonian','Finnish','French','Frisian','Galician','Georgian','German','Greek','Gujarati','Haitian Creole','Hausa','Hawaiian','Hebrew','Hindi','Hmong','Hungarian','Icelandic','Igbo','Indonesian','Irish','Italian','Japanese','Javanese','Kannada','Kazakh','Kazakhmer','Kinyarwanda','Korean','Kurdish','Kyrgyz','Lao','Latin','Latvian','Lithuanian','Luxembourgish','Macedonian','Malagasy','Malay','Malayalam','Maltese','Maori','Marathi','Mongolian','Myanmar (Burmese)','Nepali','Norwegian','Nyanja (Chichewa)','Odia (Oriya)','Pashto','Persian','Polish','Portuguese (Portugal, Brazil)','Punjabi','Romanian','Russian','Samoan','Scots Gaelic','Serbian','Sesotho','Shona','Sindhi','Sinhala (Sinhalese)','Slovak','Slovenian','Somali','Spanish','Sundanese','Swahili','Swedish','Tagalog (Filipino)','Tajik','Tamil','Tatar','Telugu','Thai','Turkish','Turkmen','Ukrainian','Urdu','Uyghur','Uzbek','Vietnamese','Welsh','Xhosa','Yiddish','Yoruba','Zulu']
-   
-    # l1 = temp[k.index(lang1)]
-    l2 = temp[k.index(lang2)]
-    blob = TextBlob(str(ip_word))
-    res = blob.translate(to=str(l2))
-    # res = blob.translate(from_lang=str(l1),to=str(l2))
-    
-    return render_template('base.html',translated_text=f'\n the translation of the text {(ip_word)} in {(l2)} is :  {res}')
+        translator = Translator(from_lang='el', to_lang=temp[k.index(lang2)])
+        res = translator.translate(ip_word)
+        # # l1 = temp[k.index(lang1)]
+        # l2 = temp[k.index(lang2)]
+        # blob = TextBlob(str(ip_word))
+        # res = blob.translate(to=str(l2))
+        # res = blob.translate(from_lang=str(l1),to=str(l2))
+    except:
+        res=ip_word  
+    return render_template('base.html',translated_text=f'\n the translated text of the word {(ip_word)} in {(lang2)} is:  {res}')
 
 if __name__=='__main__':
     app1.run(debug=True)
